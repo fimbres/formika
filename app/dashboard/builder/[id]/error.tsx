01740b45a43049da1208d09396a0c254a8d4dda9
Isaac Fimbres <fimbresisaac23@gmail.com>
2024-01-09 15:57:53 -0800

    minor text styling change

--- app/dashboard/builder/[id]/error.tsx
+++ app/dashboard/builder/[id]/error.tsx
@@ -11,7 +11,7 @@ function ErrorPage({ error }: { error: Error }) {
 
   return (
     <div className="flex w-full h-full flex-col items-center justify-center gap-4">
-      <h2 className="text-destructive text-4xl">Something went wrong!</h2>
+      <h2 className="text-4xl font-semibold">Something went wrong!</h2>
       <Button asChild>
         <Link href={"/"}>Go back to home</Link>
       </Button>
