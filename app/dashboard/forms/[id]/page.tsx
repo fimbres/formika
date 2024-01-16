f11378d18abe47eb17211c8e56f2210cb9709d93
Isaac Fimbres <fimbresisaac23@gmail.com>
2024-01-16 00:11:11 -0800

    new forms page

---
+++ app/dashboard/forms/[id]/page.tsx
@@ -0,0 +1,194 @@
+import React, { ReactNode } from "react";
+import { format, formatDistance } from "date-fns";
+
+import { GetFormById, GetFormWithSubmissions } from "@/actions/form";
+import FormLinkShare from "@/components/form-link-share";
+import VisitButton from "@/components/visit-button";
+import { ElementsType, FormElementInstance } from "@/components/form-elements";
+
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { Badge } from "@/components/ui/badge";
+import { Checkbox } from "@/components/ui/checkbox";
+import StatCard from "@/components/stat-card";
+import { CursorArrowIcon, EyeClosedIcon, EyeOpenIcon, RocketIcon, TableIcon } from "@radix-ui/react-icons";
+
+async function FormDetailPage({
+  params,
+}: {
+  params: {
+    id: string;
+  };
+}) {
+  const { id } = params;
+  const form = await GetFormById(Number(id));
+  if (!form) {
+    throw new Error("form not found");
+  }
+
+  const { visits, submissions } = form;
+
+  let submissionRate = 0;
+
+  if (visits > 0) {
+    submissionRate = (submissions / visits) * 100;
+  }
+
+  const bounceRate = 100 - submissionRate;
+
+  return (
+    <>
+      <div className="py-10 border-b border-muted">
+        <div className="flex justify-between container">
+          <h1 className="text-4xl font-bold truncate">{form.name}</h1>
+          <VisitButton shareUrl={form.shareURL} />
+        </div>
+      </div>
+      <div className="py-4 border-b border-muted">
+        <div className="container flex gap-2 items-center justify-between">
+          <FormLinkShare shareUrl={form.shareURL} />
+        </div>
+      </div>
+      <div className="w-full pt-8 gap-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 container">
+        <StatCard
+          title="Total visits"
+          icon={<EyeOpenIcon className="text-blue-600" />}
+          description="All time form visits"
+          value={visits.toLocaleString() || ""}
+          loading={false}
+        />
+
+        <StatCard
+          title="Total submissions"
+          icon={<RocketIcon className="text-yellow-600" />}
+          description="All time form submissions"
+          value={submissions.toLocaleString() || ""}
+          loading={false}
+        />
+
+        <StatCard
+          title="Submission rate"
+          icon={<CursorArrowIcon className="text-green-600" />}
+          description="Visits that result in form submission"
+          value={submissionRate.toLocaleString() + "%" || ""}
+          loading={false}
+        />
+
+        <StatCard
+          title="Bounce rate"
+          icon={<EyeClosedIcon className="text-red-600" />}
+          description="Visits that leaves without interacting"
+          value={bounceRate.toLocaleString() + "%" || ""}
+          loading={false}
+        />
+      </div>
+
+      <div className="container pt-10">
+        {/* @ts-expect-error Server Component */}
+        <SubmissionsTable id={form.id} />
+      </div>
+    </>
+  );
+}
+
+export default FormDetailPage;
+
+type Row = { [key: string]: string } & {
+  submittedAt: Date;
+};
+
+async function SubmissionsTable({ id }: { id: number }) {
+  const form = await GetFormWithSubmissions(id);
+
+  if (!form) {
+    throw new Error("form not found");
+  }
+
+  const formElements = JSON.parse(form.content) as FormElementInstance[];
+  const columns: {
+    id: string;
+    label: string;
+    required: boolean;
+    type: ElementsType;
+  }[] = [];
+
+  formElements.forEach((element) => {
+    switch (element.type) {
+      case "TextField":
+      case "NumberField":
+      case "TextAreaField":
+      case "DateField":
+      case "SelectField":
+      case "CheckboxField":
+        columns.push({
+          id: element.id,
+          label: element.extraAttributes?.label,
+          required: element.extraAttributes?.required,
+          type: element.type,
+        });
+        break;
+      default:
+        break;
+    }
+  });
+
+  const rows: Row[] = [];
+  form.FormSubmissions.forEach((submission) => {
+    const content = JSON.parse(submission.content);
+    rows.push({
+      ...content,
+      submittedAt: submission.createdAt,
+    });
+  });
+
+  return (
+    <>
+      <h1 className="text-2xl font-bold my-4">Submissions</h1>
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              {columns.map((column) => (
+                <TableHead key={column.id} className="uppercase">
+                  {column.label}
+                </TableHead>
+              ))}
+              <TableHead className="text-muted-foreground text-right uppercase">Submitted at</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {rows.map((row, index) => (
+              <TableRow key={index}>
+                {columns.map((column) => (
+                  <RowCell key={column.id} type={column.type} value={row[column.id]} />
+                ))}
+                <TableCell className="text-muted-foreground text-right">
+                  {formatDistance(row.submittedAt, new Date(), {
+                    addSuffix: true,
+                  })}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+    </>
+  );
+}
+
+function RowCell({ type, value }: { type: ElementsType; value: string }) {
+  let node: ReactNode = value;
+
+  switch (type) {
+    case "DateField":
+      if (!value) break;
+      const date = new Date(value);
+      node = <Badge variant={"outline"}>{format(date, "dd/MM/yyyy")}</Badge>;
+      break;
+    case "CheckboxField":
+      const checked = value === "true";
+      node = <Checkbox checked={checked} disabled />;
+      break;
+  }
+
+  return <TableCell>{node}</TableCell>;
+}
