077a9ef8d3262a1b60c87cb2a2283c04cc939f50
Isaac Fimbres <fimbresisaac23@gmail.com>
2024-01-16 00:12:40 -0800

    new form builder component

--- components/form-builder.tsx
+++ components/form-builder.tsx
@@ -1,34 +1,134 @@
-import React from 'react'
-import { Form } from '@prisma/client'
+"use client";
 
-import PreviewDialogButton from './preview-dialog-button'
-import SaveFormButton from './save-form-button'
-import PublishFormButton from './publish-form-button'
-import Designer from './designer'
+import React, { useEffect, useState } from "react";
+import Link from "next/link";
+import Confetti from "react-confetti";
+import { Form } from "@prisma/client";
+import { DndContext, MouseSensor, TouchSensor, useSensor, useSensors } from "@dnd-kit/core";
+import { ChevronRightIcon, ReloadIcon } from "@radix-ui/react-icons";
+
+import useDesigner from "@/hooks/use-designer";
+
+import PreviewDialogBtn from "./preview-dialog-button";
+import PublishFormBtn from "./publish-form-button";
+import SaveFormBtn from "./save-form-button";
+import Designer from "./designer";
+import DragOverlayWrapper from "./drag-overlay-wrapper.tsx";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import { toast } from "./ui/use-toast";
 
 function FormBuilder({ form }: { form: Form }) {
-  return (
-    <main className="flex flex-col w-full">
-      <nav className="flex justify-between border-b-2 p-4 gap-3 items-center">
-        <h2 className="truncate font-medium">
-          <span className="text-muted-foreground mr-2">Form:</span>
-          {form.name}
-        </h2>
-        <div className="flex items-center gap-2">
-          <PreviewDialogButton />
-          {!form.published && (
-            <>
-              <SaveFormButton id={form.id} />
-              <PublishFormButton id={form.id} />
-            </>
-          )}
-        </div>
-      </nav>
-      <div className="flex w-full flex-grow items-center justify-center relative overflow-y-auto h-[200px] bg-accent bg-[url(/paper.svg)] dark:bg-[url(/paper-dark.svg)]">
-        <Designer />
+  const { setElements, setSelectedElement } = useDesigner();
+  const [isReady, setIsReady] = useState(false);
+
+  const mouseSensor = useSensor(MouseSensor, {
+    activationConstraint: {
+      distance: 10, // 10px
+    },
+  });
+
+  const touchSensor = useSensor(TouchSensor, {
+    activationConstraint: {
+      delay: 300,
+      tolerance: 5,
+    },
+  });
+
+  const sensors = useSensors(mouseSensor, touchSensor);
+
+  useEffect(() => {
+    if (isReady) return;
+    const elements = JSON.parse(form.content);
+    setElements(elements);
+    setSelectedElement(null);
+    const readyTimeout = setTimeout(() => setIsReady(true), 500);
+    return () => clearTimeout(readyTimeout);
+  }, [form, setElements, isReady, setSelectedElement]);
+
+  if (!isReady) {
+    return (
+      <div className="flex flex-col items-center justify-center w-full h-full">
+        <ReloadIcon className="animate-spin h-12 w-12" />
       </div>
-    </main>
-  )
+    );
+  }
+
+  const shareUrl = `${window.location.origin}/submit/${form.shareURL}`;
+
+  if (form.published) {
+    return (
+      <>
+        <Confetti width={window.innerWidth} height={window.innerHeight} recycle={false} numberOfPieces={1000} />
+        <div className="flex flex-col items-center justify-center h-full w-full">
+          <div className="max-w-md">
+            <h1 className="text-center text-4xl font-bold text-primary border-b pb-2 mb-10">
+              🎊🎊 Form Published 🎊🎊
+            </h1>
+            <h2 className="text-2xl">Share this form</h2>
+            <h3 className="text-xl text-muted-foreground border-b pb-10">
+              Anyone with the link can view and submit the form
+            </h3>
+            <div className="my-4 flex flex-col gap-2 items-center w-full border-b pb-4">
+              <Input className="w-full" readOnly value={shareUrl} />
+              <Button
+                className="mt-2 w-full"
+                onClick={() => {
+                  navigator.clipboard.writeText(shareUrl);
+                  toast({
+                    title: "Copied!",
+                    description: "Link copied to clipboard",
+                  });
+                }}
+              >
+                Copy link
+              </Button>
+            </div>
+            <div className="flex justify-between">
+              <Button variant={"link"} asChild>
+                <Link href={"/dashboard"} className="gap-2">
+                  <ChevronRightIcon />
+                  Go back home
+                </Link>
+              </Button>
+              {/* <Button variant={"link"} asChild>
+                <Link href={`/dashboard/forms/${form.id}`} className="gap-2">
+                  Form details
+                  <ChevronRightIcon />
+                </Link>
+              </Button> */}
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
+
+  return (
+    <DndContext sensors={sensors}>
+      <main className="flex flex-col w-full">
+        <nav className="flex justify-between border-b-2 p-4 gap-3 items-center">
+          <h2 className="truncate font-medium">
+            <span className="text-muted-foreground mr-2">Form:</span>
+            {form.name}
+          </h2>
+          <div className="flex items-center gap-2">
+            <PreviewDialogBtn />
+            {!form.published && (
+              <>
+                <SaveFormBtn id={form.id} />
+                <PublishFormBtn id={form.id} />
+              </>
+            )}
+          </div>
+        </nav>
+        <div className="flex w-full flex-grow items-center justify-center relative overflow-y-auto h-[200px] bg-accent bg-[url(/paper.svg)] dark:bg-[url(/paper-dark.svg)]">
+          <Designer />
+        </div>
+      </main>
+      <DragOverlayWrapper />
+    </DndContext>
+  );
 }
 
-export default FormBuilder
+export default FormBuilder;
