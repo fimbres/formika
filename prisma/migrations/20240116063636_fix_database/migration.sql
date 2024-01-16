7536020a358a2b3ce86d2d9fa00096a0d25b85fb
Isaac Fimbres <fimbresisaac23@gmail.com>
2024-01-15 22:37:34 -0800

    fixes in database

---
+++ prisma/migrations/20240116063636_fix_database/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[shareURL]` on the table `Form` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[name,userId]` on the table `Form` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- CreateIndex
+CREATE UNIQUE INDEX "Form_shareURL_key" ON "Form"("shareURL");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Form_name_userId_key" ON "Form"("name", "userId");
