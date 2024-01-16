865710f555b83abe0f6d3e1e405ba6adbdbe5ae8
Isaac Fimbres <fimbresisaac23@gmail.com>
2024-01-16 00:13:13 -0800

    new form elements

---
+++ components/form-elements.tsx
@@ -0,0 +1,75 @@
+import { CheckboxFieldFormElement } from "./fields/checkbox-field";
+import { DateFieldFormElement } from "./fields/date-field";
+import { NumberFieldFormElement } from "./fields/number-field";
+import { ParagprahFieldFormElement } from "./fields/paragraph-field";
+import { SelectFieldFormElement } from "./fields/select-field";
+import { SeparatorFieldFormElement } from "./fields/separator-field";
+import { SpacerFieldFormElement } from "./fields/spacer-field";
+import { SubTitleFieldFormElement } from "./fields/subtitle-field";
+import { TextAreaFormElement } from "./fields/text-area-field";
+import { TextFieldFormElement } from "./fields/text-field";
+import { TitleFieldFormElement } from "./fields/title-field";
+
+export type ElementsType =
+  | "TextField"
+  | "TitleField"
+  | "SubTitleField"
+  | "ParagraphField"
+  | "SeparatorField"
+  | "SpacerField"
+  | "NumberField"
+  | "TextAreaField"
+  | "DateField"
+  | "SelectField"
+  | "CheckboxField";
+
+export type SubmitFunction = (key: string, value: string) => void;
+
+export type FormElement = {
+  type: ElementsType;
+
+  construct: (id: string) => FormElementInstance;
+
+  designerBtnElement: {
+    icon: React.ElementType;
+    label: string;
+  };
+
+  designerComponent: React.FC<{
+    elementInstance: FormElementInstance;
+  }>;
+  formComponent: React.FC<{
+    elementInstance: FormElementInstance;
+    submitValue?: SubmitFunction;
+    isInvalid?: boolean;
+    defaultValue?: string;
+  }>;
+  propertiesComponent: React.FC<{
+    elementInstance: FormElementInstance;
+  }>;
+
+  validate: (formElement: FormElementInstance, currentValue: string) => boolean;
+};
+
+export type FormElementInstance = {
+  id: string;
+  type: ElementsType;
+  extraAttributes?: Record<string, any>;
+};
+
+type FormElementsType = {
+  [key in ElementsType]: FormElement;
+};
+export const FormElements: FormElementsType = {
+  TextField: TextFieldFormElement,
+  TitleField: TitleFieldFormElement,
+  SubTitleField: SubTitleFieldFormElement,
+  ParagraphField: ParagprahFieldFormElement,
+  SeparatorField: SeparatorFieldFormElement,
+  SpacerField: SpacerFieldFormElement,
+  NumberField: NumberFieldFormElement,
+  TextAreaField: TextAreaFormElement,
+  DateField: DateFieldFormElement,
+  SelectField: SelectFieldFormElement,
+  CheckboxField: CheckboxFieldFormElement,
+};
